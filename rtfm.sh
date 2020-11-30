2749d4813fdc3363997f15df47a7fa56f3c4ce4c
Timothy W. Crane <677554+timothywcrane@users.noreply.github.com>
2020-11-30 02:52:38 -0600

    Restructuring
     Reformatted comments and removed unneeded symbol text in echo code

--- rtfm.sh
+++ rtfm.sh
@@ -1,57 +1,35 @@
-#!/bin/bash -x ##############################################
-# Debugging -x flag above acts as a poor man's progress bar #
-#############################################################
-
-##########################################
-# Make A Folder To Output RTFM Documents #
-##########################################
-
+#!/bin/bash -x
+# 1. Debugging -x flag above acts as a poor man's progress bar [1]
+# 2. Make A Folder To Output RTFM Documents [22]
+# 3. Copy alarm audio file into folder [23]
+# 4. Enter the folder you created [24]
+# 5. Get a list of a user's available system commands and create a text file called commandlist.txt [25]
+# 6. Create a ps file from commandlist.txt [26]
+# 7. Create a pdf document from RTFCMD.ps [27]
+# 8. Remove the ps file used to make the pdf [28]
+# 9. Then Loop through the commands in commandlist.txt to make text manual named RTFM.txt
+#        with header and informational text
+#        For each command a decorated header and important system information is sent
+#        to a text file named RTFM.txt [29..58]
+# 10. Create RTFM.ps file from RTFM.txt [59]
+# 11. Create RTFM.pdf from RTFM.ps [60]
+# 12. Optional Cleanup
+#       Remove RTFM.ps - Default [61]
+#       Remove RTFM.txt [62]
+#       Remove commandlist.txt [63]
+# 13. Announce through the terminal that the RTFM process has been completed [64]
+# 14. Optionally with audio ( with alsa-utils installed to provide the aplay command) [65]
 mkdir RTFM
-
-################################
-# Enter the folder you created #
-################################
-
+cp endbell.wav RTFM
 cd RTFM
-
-##################################################################################################
-# Get a list of a user's available system commands and create a text file called commandlist.txt #
-##################################################################################################
-
 compgen -bcegksuv |sort|uniq >> commandlist.txt
-
-###############################################
-# Create a ps file out of the commandlist.txt #
-###############################################
-
 enscript -p RTFCMD.ps commandlist.txt
-
-#############################################
-# Create a pdf document out of the ps file. #
-#############################################
-
 ps2pdf RTFCMD.ps RTFCMD.pdf
-
-# #########################################
-# Remove the ps file used to make the pdf #
-###########################################
-  
-  rm RTFCMD.ps
-  
-  
-################################################
-# Loop through the commands in commandlist.txt #
-################################################
-
+rm RTFCMD.ps
 while read p; do
-
-#
-# For Each One - Print decorated headers and important system information to a text file named RTFM.txt
   echo " " >> RTFM.txt
   echo "*********************************************************************************************" >> RTFM.txt
-  echo "*********************************************************************************************" >> RTFM.txt
   echo " INFORMATION FOR THE $p COMMAND " >> RTFM.txt
-  echo "*********************************************************************************************" >> RTFM.txt
   echo "*********************************************************************************************" >> RTFM.txt
   echo " " >> RTFM.txt
   echo " " >> RTFM.txt
@@ -72,48 +50,14 @@ while read p; do
   echo " " >> RTFM.txt
   echo " " >> RTFM.txt
   echo "********************** $p ALSO MATCHES **********************" >> RTFM.txt
-  apropos -e "$p" >> RTFM.txt
+  apropos -e "$p" >> RTFM.txt 2>&1
   echo " " >> RTFM.txt
   echo " " >> RTFM.txt
 done < commandlist.txt
-#
-##############################################
-# Create a ps file out of the RTFM text file #
-##############################################
-
 enscript -p RTFM.ps RTFM.txt
-
-#########################################
-# Create a pdf ebook out of the ps file #
-#########################################
-
 ps2pdf RTFM.ps RTFM.pdf
-
-####################
-# Optional Cleanup #
-####################
-
-############################
-# Remove ps file - Default #
-############################
-
 rm RTFM.ps
-
-#############################
-# Remove the RTFM text file #
-#############################
-
 # rm RTFM.txt
-
-###################################
-# Remove the commandlist.txt file #
-###################################
-
 # rm commandlist.txt
-
-##########################################################################
-# Announce through the terminal that the RTFM process has been completed #
-##########################################################################
-
 echo "Done. Now RTFM in your favorite ebook viewer."
-
+aplay endbell.wav
