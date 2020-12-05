43311ed201659573784b6c970b083cc0142ad455
Timothy W. Crane <677554+timothywcrane@users.noreply.github.com>
2020-12-05 05:41:17 -0600

    Main Version: Pre-Alpha 0.0.3
    Capitalized ADVENTUROUS Header

--- rtfm.sh
+++ rtfm.sh
@@ -243,7 +243,7 @@ echo " "
 #                                                                                     
 
 echo " "
-echo "           Adventurous"
+echo "           ADVENTUROUS"
 
 
 # Enscript2dropbox
